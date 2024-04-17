5324bc0c92476a0f8e2c4d3dbb240ac622c31b52
Francisco Sierra Ares <franciscosa98@educastur.es>
2024-04-17 02:42:26 +0200

    Añadidos comentarios al código. Versión 1

--- src/juego_dados/Juego_dados.java
+++ src/juego_dados/Juego_dados.java
@@ -5,19 +5,28 @@ import java.util.Random;
 public class Juego_dados {
 
     public static void main(String[] args) {
+        /**
+         * Se crean dos variables que almacenan los valores de los dados.
+         */
         int valorDado1 = 0;
         int valorDado2 = 0;
-        Random dado = new Random(); 
-        int contador = 0;
-        
+        Random dado = new Random(); // Random que se utiliza para generar los valores.
+        int contador = 0; // Contador para las tiradas
+        /**
+         * Se utiliza un bucle para realizar tiradas hasta que ambos valores sean 6.
+         */
         while (valorDado1 != 6 || valorDado2 != 6){
+            /**
+             * Se genera el valor entre 0 y el valor especificado, por esto se suma 1,
+             * para que nunca salga 0. También se imprimen los resultados.
+             */
 	        valorDado1 = dado.nextInt(6)+1;
 	        valorDado2 = dado.nextInt(6)+1;
 	        System.out.println( "Dado 1 - Dado 2");
 		System.out.println("  "+valorDado1+" 	   "+valorDado2);
 	        contador++;
         }
-        
+        // Al terminar se imprimen las tiradas.
         System.out.println("Número de tiradas: " + contador);
     }
     
