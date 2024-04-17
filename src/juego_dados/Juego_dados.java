fbc55919d2308d310aaffe530b752c9d4f22e4eb
Francisco Sierra Ares <franciscosa98@educastur.es>
2024-04-17 02:54:00 +0200

    Se añaden metodos mostrarResultado() y tirarDado(). Se corrigen comentarios

--- src/juego_dados/Juego_dados.java
+++ src/juego_dados/Juego_dados.java
@@ -16,18 +16,31 @@ public class Juego_dados {
          * Se utiliza un bucle para realizar tiradas hasta que ambos valores sean 6.
          */
         while (valorDado1 != 6 || valorDado2 != 6){
-            /**
-             * Se genera el valor entre 0 y el valor especificado, por esto se suma 1,
-             * para que nunca salga 0. También se imprimen los resultados.
-             */
-	        valorDado1 = dado.nextInt(6)+1;
-	        valorDado2 = dado.nextInt(6)+1;
-	        System.out.println( "Dado 1 - Dado 2");
-		System.out.println("  "+valorDado1+" 	   "+valorDado2);
+	        valorDado1 = tirarDado(dado);
+	        valorDado2 = tirarDado(dado);
+	        mostrarResultado(valorDado1, valorDado2);
 	        tiradas++;
         }
         // Al terminar se imprimen las tiradas.
         System.out.println("Número de tiradas: " + tiradas);
+    }
+    /**
+     * Método que imprime los resultados de las tiradas.
+     * @param valorDado1 Entero con el valor del dado 1.
+     * @param valorDado2 Entero con el valor del dado 2.
+     */
+    public static void mostrarResultado(int valorDado1, int valorDado2) {
+        System.out.println( "Dado 1 - Dado 2");
+        System.out.println("  "+valorDado1+" 	   "+valorDado2);
+    }
+    /**
+     * Método que genera el valor entre 0 y el valor especificado, por esto se suma 1,
+     * para que nunca salga 0.
+     * @param dado Objeto Random
+     * @return Valor del dado
+     */
+    public static int tirarDado(Random dado) {
+        return dado.nextInt(6)+1;
     }
     
 }
